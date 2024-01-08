bf835328186c5577b2407cc98f7908bfd526338d
Habibullah Ahmadi <habahmadi05@outlook.com>
2024-01-08 14:55:08 +0000

    moved file

--- java/arraylist/solutions.java
+++ java/arraylist/solutions.java
@@ -1,7 +1,6 @@
-/**
- * main
- */
-public class main {
+package java.arraylist;
+
+public class solutions {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }

15d585a6f1bf1f09c4bb3a529d5af6db32cd1172
Habibullah Ahmadi <habahmadi05@outlook.com>
2024-01-12 19:48:07 +0000

    Syntax Checker Solution

---
+++ java/Syntax/syntaxSolution.java
@@ -0,0 +1,27 @@
+package Java.Syntax;
+
+
+import java.util.*;
+import java.util.regex.*;
+
+public class syntaxSolution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+            
+        }
+        in.close();
+    }    
+}
+
